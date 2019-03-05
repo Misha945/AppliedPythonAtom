213f45d55ed2ed7e16919b9882736149b2a88732
Misha945 <mix.zaharov@gmail.com>
2019-03-06 00:04:14 +0300

    Second commit_det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,33 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
+    k = 0
+    while k < len(list_of_lists):
+        if len(list_of_lists[k]) != len(list_of_lists):
+            return None
+        k += 1
+    if len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    if len(list_of_lists) == 2:
+        a11 = list_of_lists[0][0]
+        a22 = list_of_lists[1][1]
+        a12 = list_of_lists[0][1]
+        a21 = list_of_lists[1][0]
+        return a11*a22-a12*a21
+    k = 0
+    result = 0
+    while k < len(list_of_lists):
+        i = 0
+        help_list = list_of_lists.copy()
+        while i < len(list_of_lists):
+            help_list[i] = list_of_lists[i].copy()
+            i += 1
+        help_list.pop(0)
+        i = 0
+        while i < len(list_of_lists) - 1:
+            help_list[i].pop(k)
+            i += 1
+        result += (-1)**k*list_of_lists[0][k]*calculate_determinant(help_list)
+        k += 1
+    return result
     raise NotImplementedError
