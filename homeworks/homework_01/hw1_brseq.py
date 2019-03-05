47c5a64404b3e56d9b254f9c7339d5231e557451
Misha945 <mix.zaharov@gmail.com>
2019-03-06 00:58:38 +0300

    minor fixes, for loop

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -6,7 +6,8 @@ def is_bracket_correct(input_string):
     a = 0
     b = 0
     c = 0
-    for i in input_string:
+    i = 0
+    while i < len(input_string):
         if input_string[i] == '(':
             a += 1
         if input_string[i] == '[':
@@ -21,6 +22,7 @@ def is_bracket_correct(input_string):
             c -= 1
         if (a < 0) or (b < 0) or (c < 0):
             return False
+        i += 1
     if (a**2 + b**2 + c**2) == 0:
         return True
     else:
