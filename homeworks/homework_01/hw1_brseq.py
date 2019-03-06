4e5f3f920a7fba9fe9c40d41860c75fcc7c48a1c
Misha945 <mix.zaharov@gmail.com>
2019-03-06 15:15:21 +0300

    The Test, please, agree

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,28 +3,23 @@
 
 
 def is_bracket_correct(input_string):
-    a = 0
-    b = 0
-    c = 0
+    stack = []
     i = 0
     while i < len(input_string):
-        if input_string[i] == '(':
-            a += 1
-        if input_string[i] == '[':
-            b += 1
-        if input_string[i] == '{':
-            c += 1
-        if input_string[i] == ')':
-            a -= 1
-        if input_string[i] == ']':
-            b -= 1
-        if input_string[i] == '}':
-            c -= 1
-        if (a < 0) or (b < 0) or (c < 0):
-            return False
+        if (input_string[i] == '(' or
+           input_string[i] == '[' or
+           input_string[i] == '{'):
+            stack.append(input_string[i])
+        else:
+            if not stack:
+                return False
+            if not((input_string[i] == ')' and stack.pop() == '(')
+               or (input_string[i] == ']' and stack.pop() == '[')
+               or (input_string[i] == '}' and stack.pop() == '{')):
+                return False
         i += 1
-    if (a**2 + b**2 + c**2) == 0:
-        return True
-    else:
+    if stack:
         return False
+    else:
+        return True
     raise NotImplementedError
