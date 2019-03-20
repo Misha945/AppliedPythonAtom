e452ad30943b67ddce820c4cfbe278fa17c26365
Misha945 <mix.zaharov@gmail.com>
2019-03-20 21:25:56 +0300

    :)

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -6,27 +6,23 @@ class TEventStats:
     FIVE_MIN = 300
 
     def __init__(self):
-        # TODO: реализовать метод
+        self.dict = {}
         raise NotImplementedError
 
     def register_event(self, user_id, time):
-        """
-        Этот метод регистрирует событие активности пользователя.
-        :param user_id: идентификатор пользователя
-        :param time: время (timestamp)
-        :return: None
-        """
-        # TODO: реализовать метод
+        if user_id in self.dict.keys():
+            self.dict[user_id].append(time)
+        else:
+            self.dict[user_id] = [time]
         raise NotImplementedError
 
     def query(self, count, time):
-        """
-        Этот метод отвечает на запросы.
-        Возвращает количество пользователей, которые за последние 5 минут
-        (на полуинтервале времени (time - 5 min, time]), совершили ровно count действий
-        :param count: количество действий
-        :param time: время для рассчета интервала
-        :return: activity_count: int
-        """
-        # TODO: реализовать метод
+        res = []
+        for i, k in self.dict.items():
+            s = sum(time - 300 < x < time for x in k)
+            if s == 0 and count == 0:
+                continue
+            if s == count:
+                res.append(i)
+        return len(res)
         raise NotImplementedError
